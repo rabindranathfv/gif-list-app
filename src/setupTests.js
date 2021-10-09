67927c9eaab8e640b02cc2987ec5ee44b648f36a
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-10-09 19:32:35 +0200

    delete unseles file, testing using jest dom extended

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,9 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
 
-import {createSerializer} from 'enzyme-to-json';
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// imports testing using ENZYME
+// import {createSerializer} from 'enzyme-to-json';
+// import Enzyme from 'enzyme';
+// import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+// Enzyme.configure({ adapter: new Adapter() });
+// expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+import '@testing-library/jest-dom/extend-expect';
