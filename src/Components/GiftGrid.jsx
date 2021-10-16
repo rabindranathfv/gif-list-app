ec53804eb7576be2ac11a50ebb0e7ca18d998712
Rabindranath Ferreira <rabindranathucv@gmail.com>
2021-10-16 23:29:45 +0200

    use fetch for get data from ghipy API

--- src/Components/GiftGrid.jsx
+++ src/Components/GiftGrid.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes, { number, string } from 'prop-types';
+
+const API_URL = `https://api.giphy.com/v1/gifs/search`;
+const API_KEY = `6ijpJmhvX9OshRF2lAHuBYHojzB9bZjX`;
 
 export const GiftGrid = props => {
-    const { category } = props;
+    const { category, searchCategory } = props;
+
+    const getGifts = async() => {
+        const urlQuery = `${API_URL}?q=${searchCategory}&limit=10&api_key=${API_KEY}`
+        const resp = await fetch( urlQuery);
+        const { data } = await resp.json();
+        
+        const giftItem = data.map( gif => {
+            return { 
+                id: gif.id,
+                title: gif.title,
+                url:  gif.images.downsized_medium.url
+            }
+        })
+        console.log('fetch data', giftItem);
+
+    }
+
+    getGifts();
     return (
         <div>
             <ul>
@@ -13,5 +34,8 @@ export const GiftGrid = props => {
 }
 
 GiftGrid.propTypes = {
-
+    category: PropTypes.shape({
+        id: number,
+        name: string,
+      }).isRequired,
 }
--- src/Components/ListGift.jsx
+++ src/Components/ListGift.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes, { number, string } from 'prop-types'
 import { GiftGrid  } from './GiftGrid';
 
 export const ListGift = (props) => {
@@ -9,10 +10,17 @@ export const ListGift = (props) => {
             <ul>
             {
                 gifts.map( cat => {
-                    return <GiftGrid key={ cat.id } category={cat} />
+                    return <GiftGrid key={ cat.id } category={cat} searchCategory={'dragon ball'}/>
                 })
             }
             </ul>
         </div>
     )
+}
+
+ListGift.propTypes = {
+    gifts: PropTypes.arrayOf(PropTypes.shape({
+        id: number,
+        name: string
+    })).isRequired
 }
