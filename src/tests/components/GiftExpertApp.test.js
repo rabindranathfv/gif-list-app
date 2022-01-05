234f2950d2034ec9271af92811da80f0856cda3a
Rabindranath Ferreira <ferreiralox@gmail.com>
2022-01-05 14:06:21 +0100

    add GiftExpert app unit test

--- src/tests/components/GiftExpertApp.test.js
+++ src/tests/components/GiftExpertApp.test.js
@@ -3,6 +3,8 @@ import React from 'react';
 import GiftExpertApp from '../../Components/GiftExpertApp';
 
 describe('GiftExpertApp', () => {
+    const categoryMock = 'dragon ball';
+    const setCategory = jest.fn();
     let wrapper = shallow( <GiftExpertApp />);
 
     beforeEach( () => {
@@ -14,5 +16,17 @@ describe('GiftExpertApp', () => {
         expect( wrapper).toMatchSnapshot();
     })
     
+    test('should render AddCategory component', () => {
+        const AddCategory = wrapper.find('AddCategory');
+        AddCategory.debug();
+        expect(AddCategory).toBeDefined();
+    })
+    
+    test('should render GiftGrid component', () => {
+        const GiftGrid = wrapper.find('GiftGrid');
+        GiftGrid.debug();
+        expect(GiftGrid).toBeDefined();
+        expect(GiftGrid.prop('category')).toEqual( [ categoryMock]);
+    })
 
 })
